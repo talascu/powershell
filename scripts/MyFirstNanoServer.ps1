10037c84d088eea005850aa6e0c6995e3f13a002
Tudor A. Lascu <talascu@gmail.com>
2015-10-16 16:22:19 +0200

    added MyFirstNanoServer.ps1 script to branch master

---
+++ scripts/MyFirstNanoServer.ps1
@@ -0,0 +1,72 @@
+﻿$nanoComputerName = 'NewNano'
+
+# enter the path where the Windows Server Technical Preview ISO has been mounted
+$mediaPath = 'G:\'
+
+# setting up the directory in which the files will be copied and necessary directories will be created
+$workingDirectoryPath = 'C:\temp\'
+$workingDirectoryName = 'NanoServer'
+
+$workingDirectory = Join-Path $workingDirectoryPath $workingDirectoryName
+
+if ( -not (Test-Path -Path $workingDirectory) ) {
+    New-Item -Path $workingDirectoryPath -ItemType Directory -Name $workingDirectoryName
+}
+
+cd $workingDirectory
+
+# download necessary scripts, see 
+# http://blogs.technet.com/b/nanoserver/archive/2015/06/16/powershell-script-to-build-your-nano-server-image.aspx
+$ConvertWindowsImageScript = 'https://gallery.technet.microsoft.com/scriptcenter/Convert-WindowsImageps1-0fe23a8f/file/59237/7/Convert-WindowsImage.ps1'
+$NanoServerScript = 'http://blogs.technet.com/cfs-filesystemfile.ashx/__key/telligent-evolution-components-attachments/01-10474-00-00-03-65-09-88/NanoServer.ps1'
+$ConvertWindowsImageScriptDest = Join-Path $workingDirectory 'Convert-WindowsImage.ps1'
+$NanoServerScriptDest = Join-Path $workingDirectory 'NanoServer.ps1'
+Invoke-WebRequest $ConvertWindowsImageScript -OutFile $ConvertWindowsImageScriptDest
+Invoke-WebRequest $NanoServerScript -OutFile $NanoServerScriptDest
+
+
+# for non US-localized computer, in NanoServer.ps1 script replace line
+# [String]$Language = [System.Globalization.CultureInfo]::CurrentCulture.Name.ToLower(),
+# with line     
+# [String]$Language = "en-us",
+if ( (Get-Culture) -ne "en-us" ) {
+    (Get-Content .\NanoServer.ps1).replace('[System.Globalization.CultureInfo]::CurrentCulture.Name.ToLower()', '"en-us"') | Set-Content .\NanoServer.ps1
+}
+
+# avoid digital signature check only for current session
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+# dot source the modified NanoServer.ps1 script, MyNanoServer.ps1
+. .\NanoServer.ps1
+
+New-NanoServerImage -MediaPath $mediaPath -BasePath .\Base -TargetPath .\FirstSteps -ComputerName $nanoComputerName -GuestDrivers -EnableIPDisplayOnBoot
+
+# create and boot a Nano Server VM
+$VmName = "FirstNanoVM"
+$VmMemory = 512MB
+$VhdRootPath = Join-Path $workingDirectory "FirstSteps"
+$VhdName = 'FirstSteps.vhd'
+$VhdPath = Join-Path $VhdRootPath $VhdName
+$switchName = 'Virtual Switch'
+New-VM –Name $VmName –MemoryStartupBytes $VmMemory –VHDPath $VhdPath -SwitchName $switchName
+Start-VM -Name $VmName
+
+# to enable  Powershell remoting in a non-domain scenario, add the IP address of the Nano Server to the trusted hosts list
+$ip = “192.168.1.4”
+Enable-PSRemoting -SkipNetworkProfileCheck -Force
+Get-Item wsman:\localhost\Client\TrustedHosts
+Set-Item WSMan:\localhost\Client\TrustedHosts $ip -Concatenate -Force
+#Set-Item WSMan:\localhost\Client\TrustedHosts -Value "192.168.1.4" -Force
+
+# first session on Nano Server through Powershell remoting
+$user = “$ip\Administrator”
+Enter-PSSession -ComputerName $ip -Credential $user
+# do some work
+# try to download file from Internet TODO
+Exit-PSSession
+
+# run commands remotely
+Invoke-Command -ComputerName $ip -Credential $user -ScriptBlock {Get-Culture}
+
+
+Stop-VM -Name $VmName
