0e21e95ff1fa4b99e6d96ffa82318fd26ff93d64
Tudor A. Lascu <talascu@gmail.com>
2015-10-19 09:45:08 +0200

    fresh script version to update a Lync 2013 Front End server

---
+++ scripts/UpdateLync13FrontEnd.ps1
@@ -0,0 +1,52 @@
+﻿# script skeleton to update a Lync 2013 Front End server
+
+Import-Module Lync
+
+# address to download Lync Update Installer
+# http://www.microsoft.com/en-us/download/details.aspx?id=36820
+# address to download SQL Server patch
+# https://support.microsoft.com/en-us/kb/3072100
+
+$computerName = 'FE01.contoso.com'
+$remotePath = '\\share\lync\patching'
+$fileName = 'LyncServerUpdateInstaller.exe'
+$localDirectory = 'lync-patch-october-2015'
+$localPath = 'C:\temp'
+
+# Step 1: verify that the FE pool is ready to be updated
+# N.B. this command must be run locally on a Front End server in the pool , cannot be run remotely
+Get-CSPoolUpgradeReadinessState
+
+# Step 2: stop Lync services on one of the FE server
+Stop-CSWindowsService -Graceful
+
+# Step 3: check that Lync services are stopped and that no more connections are active on the FE server 
+Get-CSWindowsService
+
+# Step 4: run the Cumulative Update installer
+# we copy the  installer from a shared folder to the FE server
+$LyncInstallerRemote = Join-Path $remotePath $fileName
+$localInstallerPath = Join-Path $localPath $localDirectory
+if ( -not (Test-Path $localInstallerPath) ) {
+    New-Item -Path $localPath -ItemType Directory -Name $localDirectory
+}
+Copy-Item $LyncInstallerRemote -Destination $localInstallerPath
+$localLyncInstaller = Join-Path $localInstallerPath $fileName
+# run the Cumulative Update installer
+$localLyncInstaller
+
+# Step 5: disable automatic restart of Lync services on system reboot
+Get-CSWindowsService | Set-Service -StartupType 'Disabled'
+
+# Step 6: apply Windows Update and SQL server update
+#$SqlServerUpdatePath = 'C:\temp'
+#$SqlServerUpdateName = 'SQLEXPR_x64_ENU.exe'
+#$SqlServerUpdateInstaller = Join-Path $SqlServerUpdatePath $SqlServerUpdateName
+#$SqlServerUpdateInstaller /ACTION=Patch /allinstances /IAcceptSQLServerLicenseTerms
+#.\SQLEXPR_x64_ENU.exe /ACTION=Patch /allinstances /IAcceptSQLServerLicenseTerms
+
+# Step 7: re-enable automatic startup of Lync services on system start
+Get-CSWindowsService | Set-Service -StartupType 'Automatic'
+
+# Step 8: restart all Lync services
+Get-CSWindowsService | Start-Service
